2b5c7ced09cdc5355938f282d8c439475590c782
Andrew Huth <andrew@huth.me>
2021-10-16 12:55:38 -0700

    Implement <Select>

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,50 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <CustomSelect>
+        {displayedValue}
+        <Icon id="chevron-down" size={24} strokeWidth={2} />
+      </CustomSelect>
+    </Wrapper>
   );
 };
 
 export default Select;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const CustomSelect = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 12px 16px;
+  gap: 16px;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+
+  ${NativeSelect}:hover + & {
+    color: black;
+  }
+
+  ${NativeSelect}:focus + & {
+    outline: 2px solid #4374CB;
+    outline: 2px auto -webkit-focus-ring-color;
+  }
+`;
